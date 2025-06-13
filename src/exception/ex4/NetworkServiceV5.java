025ad49a451dcd30cc641f6a63f2ee8c9605ce51
INJOON CHOI <dlswns8679@naver.com>
2025-06-14 03:00:58 +0900

    네트워크 클래스 - 강의들은것 정리 및 실습

--- src/exception/ex4/NetworkServiceV5.java
+++ src/exception/ex4/NetworkServiceV5.java
@@ -10,7 +10,7 @@ public class NetworkServiceV5 {
             client.send(data);
         } catch (Exception e) {
             System.out.println("[예외 확인]: " + e.getMessage());
-            throw e;wwwww
+            throw e;
         }
     }
 }
